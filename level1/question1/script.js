aa30ef10878c5804fe82c0a28b112c47fea33be5
Glenn Key <glennkey@Glenns-MacBook-Pro.local>
2019-10-02 18:00:15 +0200

    added the form, and a function that checks wheter the names are not empty - Also added a required in the html

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,13 @@
 //Use RegEx to validate form
+
+//Names not empty
+function validateNames() {
+    var x = document.forms["form"]["formName"].value;
+    if (x == "") {
+      alert("Names must be filled out");
+      return false;
+    }
+  };
+
+
+  
